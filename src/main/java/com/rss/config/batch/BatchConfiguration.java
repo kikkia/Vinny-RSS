a588a178f77d85c6fe2e536f192158545c956863
JessWalters <jessw@iastate.edu>
2020-09-18 08:56:19 -0500

    metrics service

--- src/main/java/com/rss/config/batch/BatchConfiguration.java
+++ src/main/java/com/rss/config/batch/BatchConfiguration.java
@@ -75,7 +75,7 @@ public class BatchConfiguration extends DefaultBatchConfigurer {
         this.metricsService = metricsService;
         this.redditRssProcessor = new RedditRssProcessor(repository, httpClient);
         this.chanRssProcessor = new ChanRssProcessor(repository);
-        this.youtubeRssProcessor = new YoutubeRssProcessor(repository, httpClient, authProperties);
+        this.youtubeRssProcessor = new YoutubeRssProcessor(repository, httpClient, authProperties, metricsService);
         this.twitterRssProcessor = new TwitterRssProcessor(repository, nitterPath);
         this.twitchRssProcessor = new TwitchRssProcessor(repository, twitchClientId, httpClient);
         this.runIdIncrementer = new RunIdIncrementer();
