a054ccc930259849c53b99f905e757f367f7e573
kikkia <sabledawn@gmail.com>
2023-07-23 13:46:29 -0700

    log rss updates sent

--- src/main/java/com/rss/batch/RssSubscriptionWriter.kt
+++ src/main/java/com/rss/batch/RssSubscriptionWriter.kt
@@ -1,17 +1,22 @@
 package com.rss.batch
 
 import com.rss.clients.MessagingClient
+import com.rss.model.RssProvider
 import com.rss.model.RssUpdate
+import com.rss.utils.DislogLogger
 import org.springframework.batch.item.ItemWriter
 
 class RssSubscriptionWriter(private val messagingClient: MessagingClient) : ItemWriter<List<RssUpdate?>> {
+
+    private val logger = DislogLogger(this.javaClass)
     @Throws(Exception::class)
     override fun write(list: List<List<RssUpdate?>>) {
         if (list.isEmpty())
             return
         for (updateList in list) {
             for (update in updateList) {
-                messagingClient.sendRssUpdate(update!!)
+                logger.info("Publishing update for ${update!!.displayName} + ${update.url}")
+                messagingClient.sendRssUpdate(update)
             }
         }
         print(list)
